0af1df10d684d82f806e08c7b3a9094aba40e525
James Hill <j.g.hill@zoho.com>
2021-11-16 05:59:28 +0000

    Updated the Help tab

--- R/mod_tabitem_help.R
+++ R/mod_tabitem_help.R
@@ -8,15 +8,19 @@ mod_tabitem_help_ui <- function() {
   bs4Dash::tabItem(
     tabName = "tab_help",
     shiny::div(
-      class = "jumbotron",
+      class = "jumbotron elevation-4",
       shiny::h1(class = "display-4", "NBA Free Throws"),
       shiny::p(
         class = "lead",
-        "This application demonstrates a Shiny application with NBA data."
+        "This application demonstrates a Shiny application with NBA data"
       ),
       shiny::hr(class = "my-4"),
-      shiny::p(
-        "The following features exist for different users:"
+      shiny::p("The following features exist for different users:"),
+      shiny::tags$ul(
+        shiny::tags$li("Stats tab for company owner"),
+        shiny::tags$li("Strategy tab for team coach"),
+        shiny::tags$li("Chart tab for data analyst"),
+        shiny::tags$li("Table tab to dig deeper")
       )
     )
   )
